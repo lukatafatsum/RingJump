69eec13b4b0aaab22a8577a73f875cc455994eb9
Selman <76404716+lukatafatsum@users.noreply.github.com>
2021-03-01 19:41:11 +0300

    singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        public static GameManager Instance;
     }
 
     // Update is called once per frame
